8303a699b1cbb361e6ae65ef7d54e306b5419b44
Nischaya Sharma <nischaya.the.gr8@gmail.com>
2018-11-29 22:53:58 +0530

    feat: Adding MultiThreading for multi client server.

--- socket-codes/server/src/server.py
+++ socket-codes/server/src/server.py
@@ -1,22 +1,34 @@
 import socket;
+import sys;
+import thread;
 
+numOfThreads = 0;
 s = socket.socket();
 port = 12345;
 s.bind(('', port));
 s.listen(5);
-clients = [];
-addr = [];
-while True:
-    c, address = s.accept();
-    clients.append(c);
-    addr.append(address);
-    print "Socket Up and running with a connection from",addr;
-    for i in range(len(clients)):
-        rcvdData = clients[i].recv(1024).decode();
-    print "S:",rcvdData;
-    sendData = raw_input("N: ");
-    for i in range(len(clients)):
-        clients[i].send(sendData.encode());
-    if(sendData == "Bye" or sendData == "bye"):
-        break;
-c.close();
+def main():
+    try:
+        global numOfThreads;
+        numOfThreads += 1;
+        print "Entering main."
+        c, addr = s.accept();
+        print "Socket Up and running with a connection from",addr;
+        while True:
+            rcvdData = c.recv(1024).decode();
+            print "S:",rcvdData;
+            sendData = raw_input("N: ");
+            c.send(sendData.encode());
+            if(sendData == "Bye" or sendData == "bye"):
+                break;
+        c.close();
+        numOfThreads -= 1;
+    except KeyboardInterrupt:
+        print "Closing Connection and freeing the port."
+        c.close();
+        sys.exit();
+
+thread.start_new_thread(main,());
+while 1:
+    pass;
+print "Exited Thread";
