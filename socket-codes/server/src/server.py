948e55cbd0f68aaed15fd22f6dc54df93312a44f
Nischaya Sharma <nischaya.the.gr8@gmail.com>
2018-11-29 22:00:52 +0530

    feat: Multi-Client acceptance(really bad code)

--- socket-codes/server/src/server.py
+++ socket-codes/server/src/server.py
@@ -4,13 +4,19 @@ s = socket.socket();
 port = 12345;
 s.bind(('', port));
 s.listen(5);
-c, addr = s.accept();
-print "Socket Up and running with a connection from",addr;
+clients = [];
+addr = [];
 while True:
-    rcvdData = c.recv(1024).decode();
+    c, address = s.accept();
+    clients.append(c);
+    addr.append(address);
+    print "Socket Up and running with a connection from",addr;
+    for i in range(len(clients)):
+        rcvdData = clients[i].recv(1024).decode();
     print "S:",rcvdData;
     sendData = raw_input("N: ");
-    c.send(sendData.encode());
+    for i in range(len(clients)):
+        clients[i].send(sendData.encode());
     if(sendData == "Bye" or sendData == "bye"):
         break;
 c.close();
