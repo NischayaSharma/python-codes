439b30cc3c6b78342147f74c48e98409f6181c1e
Nischaya Sharma <nischaya.the.gr8@gmail.com>
2018-11-29 23:25:52 +0530

    fix: Handeling Ctrl+C in client.py

--- socket-codes/client/src/client.py
+++ socket-codes/client/src/client.py
@@ -1,11 +1,16 @@
 import socket;
 
-s = socket.socket();
-s.connect(('127.0.0.1',12345));
-while True:
-    str = raw_input("S: ");
-    s.send(str.encode());
-    if(str == "Bye" or str == "bye"):
-        break;
-    print "N:",s.recv(1024).decode();
-s.close();
+try:
+    s = socket.socket();
+    s.connect(('127.0.0.1',12345));
+    while True:
+        str = raw_input("S: ");
+        s.send(str.encode());
+        if(str == "Bye" or str == "bye"):
+            break;
+        print "N:",s.recv(1024).decode();
+    s.close();
+except KeyboardInterrupt:
+    print "Closing Connection and freeing the port."
+    s.close();
+    sys.exit();
