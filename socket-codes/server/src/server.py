1fc2406fecf64b9e9a17c5817952ca7a211bf85d
Nischaya Sharma <nischaya.the.gr8@gmail.com>
2018-11-29 19:38:10 +0530

    fix: Minor Changes On server.py feat: Solution to recurring decimals problem

--- socket-codes/server/src/server.py
+++ socket-codes/server/src/server.py
@@ -1,14 +1,11 @@
 import socket;
 
 s = socket.socket();
-print "Socket successfully created";
 port = 12345;
 s.bind(('', port));
-print "socket binded to %s" %(port);
 s.listen(5);
-print "socket is listening";
 c, addr = s.accept();
-print 'Got connection from', addr;
+print "Socket Up and running with a connection from",addr;
 while True:
     rcvdData = c.recv(1024).decode();
     print "S:",rcvdData;
---
+++ src/RecurringDecimals.py
@@ -0,0 +1,4 @@
+for i in range(1,1000):
+    num = i*1.0;
+    decNum = 1.0/num;
+    strDecNum = str(decNum);
