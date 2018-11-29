915665cceebb58e0ff5ce89a47605144b16204f7
Nischaya Sharma <nischaya.the.gr8@gmail.com>
2018-11-29 23:33:53 +0530

    feat: Above Average Multi Threading

--- socket-codes/server/src/server.py
+++ socket-codes/server/src/server.py
@@ -7,23 +7,23 @@ try:
     port = 12345;
     s.bind(('', port));
     s.listen(5);
-    numOfClients = 0;
+    i = 0;
 
-    def main(c):
-        global numOfClients;
+    def main(c,numOfThread):
         while True:
             rcvdData = c.recv(1024).decode();
-            print "S:",rcvdData;
+            print "Thread:", numOfThread, "S:", rcvdData;
             sendData = raw_input("N: ");
             c.send(sendData.encode());
             if(sendData == "Bye" or sendData == "bye"):
                 break;
         c.close();
 
-    while 1:
+    while (i>=0):
         c, addr = s.accept();
         print "Socket Up and running with a connection from",addr;
-        thread.start_new_thread(main,(c,));
+        thread.start_new_thread(main,(c,i,));
+        i+=1;
 
 except KeyboardInterrupt:
     print "Closing Connection and freeing the port."
