958a6b618b148848e14533618902f0f34775a61f
Nischaya Sharma <nischaya.the.gr8@gmail.com>
2018-12-02 23:03:38 +0530

    fix: Deleting an element from the file

--- src/FileIO.py
+++ src/FileIO.py
@@ -35,32 +35,66 @@ def fileUpdate(lineNum):
         fwu.write(line);
     fwu.close();
 
+def fileDelete(lineNum):
+    empFName, empLName, empSalary,  empEmailId = raw_input("Enter employee first name: "), raw_input("Enter employee last name: "), raw_input("Enter employee salary: "), raw_input("Enter employee Email ID: ");
+    fr = open(string, "r");
+    lines = fw.readlines();
+    fr.close();
+    fwu = open(string, "w");
+    lines.pop(lineNum);
+    for line in lines:
+        fwu.write(line);
+    fwu.close();
+    frc = open(os.path.dirname(os.path.realpath(__file__))+"/counter.txt","r");
+    counter = int(frc.read());
+    frc.close();
+    fwc = open(os.path.dirname(os.path.realpath(__file__))+"/counter.txt","w");
+    fwc.write(str(counter-1)+"\n");
+    fwc.close();
+
 def filePrint():
     fw.seek(0);
     lines = fw.readlines();
     print "The File: "
     for line in lines:
         print line;
-def main():
+
+def crud():
+
+    #Creating and writing to file
     fw.seek(0);
     numOfEmployees = input("Enter the number of employees: ");
     fileInput(fw,numOfEmployees);
     filePrint();
+
+    #Reading the file for an element
     fw.seek(0);
     searchStr = raw_input("Enter the name of the employee you want to search: ");
     fileSearch(fw,searchStr);
     filePrint();
+
+    #Updating an entry in the file
     fw.seek(0);
     lineNum = int(input("Enter the entry you want to update(The entries start with 0 so the 1st entry is 0th entry and so on.): "));
     fileUpdate(lineNum);
     filePrint();
+
+    #Deleting an entry from the file
+    fw.seek(0);
+    lineNum = int(input("Enter the entry you want to delete(The entries start with 0 so the 1st entry is 0th entry and so on.): "));
+    fileDelete(lineNum);
+    filePrint();
+
+    #Closing the file
     fw.close();
 
-try:
-    string = os.path.dirname(os.path.realpath(__file__))+'/'+FileName;
-    fw = open(string, "a+");
-    main();
-except Exception as e:
-    print e;
-    fw.close();
-    sys.exit();
+def main():
+    try:
+        string = os.path.dirname(os.path.realpath(__file__))+'/'+FileName;
+        fw = open(string, "a+");
+        crud();
+    except Exception as e:
+        print e;
+        fw.close();
+        sys.exit();
+main();
