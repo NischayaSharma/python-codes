e476bca7505b1b938fadaa347c741d43148c6b30
Ubuntu <ubuntu@ip-172-31-37-89.us-east-2.compute.internal>
2019-05-15 04:58:36 +0000

    Server Client system not working on internet

--- socket-codes/client/src/client.py
+++ socket-codes/client/src/client.py
@@ -3,7 +3,7 @@ import sys;
 
 try:
     s = socket.socket();
-    s.connect(('127.0.0.1',12345));
+    s.connect(('18.219.110.96',12345));
     while True:
         str = raw_input("S: ");
         s.send(str.encode());
